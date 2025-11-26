fbd2ad028820038690ed0a3ce995645f971e37b8
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2025-11-26 18:02:54 -0300

    Refactor: Rename and enhance JPA configuration for better maintainability
    Renamed `JpaConfiguration` to `SpringDataJpaConfiguration` and introduced auditing capabilities with `DateTimeProvider` and `AuditorAware` beans. Enhanced configuration to support UUID-based auditing and align with best practices for Spring Data JPA.

---
+++ src/main/java/com/github/allisson95/algashop/ordering/infrastructure/persistence/configuration/SpringDataJpaConfiguration.java
@@ -0,0 +1,34 @@
+package com.github.allisson95.algashop.ordering.infrastructure.persistence.configuration;
+
+import io.hypersistence.utils.spring.repository.BaseJpaRepositoryImpl;
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware", dateTimeProviderRef = "dateTimeProvider")
+@EnableJpaRepositories(
+        basePackages = { "com.github.allisson95.algashop.ordering.infrastructure.persistence" },
+        repositoryBaseClass = BaseJpaRepositoryImpl.class
+)
+public class SpringDataJpaConfiguration {
+
+    @Bean
+    AuditorAware<@NonNull UUID> auditorAware() {
+        return () -> Optional.of(UUID.randomUUID());
+    }
+
+    @Bean
+    DateTimeProvider dateTimeProvider() {
+        return () -> Optional.of(Instant.now());
+    }
+
+}
