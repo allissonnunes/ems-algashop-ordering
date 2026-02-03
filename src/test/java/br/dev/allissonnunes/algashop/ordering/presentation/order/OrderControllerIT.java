8a913dad100844bd92f515522ad5ecfe375f4be3
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2026-02-03 18:02:26 -0300

    Verify `OrderControllerIT` persists orders after creation
    - Extended `OrderControllerIT` to include assertions for order persistence using `OrderPersistenceEntityRepository`.
    - Updated response validations to ensure the order ID and customer ID are returned in the `buyNow` endpoint response.

--- src/test/java/br/dev/allissonnunes/algashop/ordering/presentation/order/OrderControllerIT.java
+++ src/test/java/br/dev/allissonnunes/algashop/ordering/presentation/order/OrderControllerIT.java
@@ -1,7 +1,9 @@
 package br.dev.allissonnunes.algashop.ordering.presentation.order;
 
+import br.dev.allissonnunes.algashop.ordering.domain.model.order.OrderId;
 import br.dev.allissonnunes.algashop.ordering.infrastructure.persistence.customer.CustomerPersistenceEntityRepository;
 import br.dev.allissonnunes.algashop.ordering.infrastructure.persistence.customer.CustomerPersistenceEntityTestDataBuilder;
+import br.dev.allissonnunes.algashop.ordering.infrastructure.persistence.order.OrderPersistenceEntityRepository;
 import br.dev.allissonnunes.algashop.ordering.utils.JsonFileSource;
 import io.restassured.RestAssured;
 import io.restassured.config.EncoderConfig;
@@ -9,6 +11,7 @@ import io.restassured.config.JsonConfig;
 import io.restassured.path.json.config.JsonPathConfig;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,8 @@ import org.springframework.http.MediaType;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderControllerIT {
@@ -32,6 +37,9 @@ class OrderControllerIT {
     private CustomerPersistenceEntityRepository customerRepository;
 
     private final UUID customerId = UUID.fromString("6e148bd5-47f6-4022-b9da-07cfaa294f7a");
+
+    @Autowired
+    private OrderPersistenceEntityRepository orderRepository;
 
     @BeforeEach
     void setUp() {
@@ -73,9 +81,18 @@ class OrderControllerIT {
                 .spec(request)
                 .post("/api/v1/orders");
 
-        response.then()
+        final String createdOrderId = response.then()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .statusCode(HttpStatus.CREATED.value());
+                .statusCode(HttpStatus.CREATED.value())
+                .body(
+                        "id", Matchers.not(Matchers.blankString()),
+                        "customer.id", Matchers.equalTo(customerId.toString())
+                )
+                .extract()
+                .jsonPath().getString("id");
+
+        final boolean isOrderPersisted = orderRepository.existsById(new OrderId(createdOrderId).value().toLong());
+        assertThat(isOrderPersisted).isTrue();
     }
 
     @ParameterizedTest
