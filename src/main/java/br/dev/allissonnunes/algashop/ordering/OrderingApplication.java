5ca4586ee90ca68726d0590303f5a42db09c1a28
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2026-02-04 11:12:21 -0300

    Set default timezone to UTC for all microservices
    Aligned the timezone configuration across Product Catalog, Ordering, and Billing microservices by setting the default timezone to UTC. This ensures consistent time handling across the application.

--- src/main/java/br/dev/allissonnunes/algashop/ordering/OrderingApplication.java
+++ src/main/java/br/dev/allissonnunes/algashop/ordering/OrderingApplication.java
@@ -3,10 +3,14 @@ package br.dev.allissonnunes.algashop.ordering;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class OrderingApplication {
 
     static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
         SpringApplication.run(OrderingApplication.class, args);
     }
 
